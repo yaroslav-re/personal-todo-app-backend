c4b7ae8e20fb1d37a60bc7020e79675537f87201
yaroslav-re <repinyar7@mail.ru>
2022-07-18 21:23:28 +0500

    mongo.js

--- index.js
+++ index.js
@@ -71,5 +71,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
-
-// остановились на методе delete
--- mongo.js
+++ mongo.js
@@ -9,4 +9,31 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://phonebookdb:${password}@cluster1-lm71m.mongodb.net/phoneDB?retryWrites=true&w=majority`;
+const url = `mongodb+srv://yarik:${password}@cluster1-lm71m.mongodb.net/phoneDB?retryWrites=true&w=majority`;
+
+const todoSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected");
+    const todo = new Todo({
+      content: "its alive",
+      date: new Date(),
+      important: true,
+    });
+    return todo.save();
+  })
+  .then(() => {
+    console.log("todo saved");
+    return mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
